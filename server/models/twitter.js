cc8752f1b3728b04ce09f975d9c350e286719f77
imatmati <ivan.matmati@laposte.net>
2017-06-18 17:41:31 +0200

    Remove of twitter access data

--- server/models/twitter.js
+++ server/models/twitter.js
@@ -1,13 +1,12 @@
 'use strict';
 var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: 'cIanIXE9zk9hxD0CewwgBTQHl',
-  consumer_secret: 'VpJO50Gks3iOSQLkUKdgOD59qJcxeNea0sHtxjJv5x98TCh08u',
-  access_token_key: '735129973544095744-6lA0asmCKYEBZXmf8YUTNk96XVCTgYV',
-  access_token_secret: 'Qog6HrLHvSiBbwR1gd2ea4G0zK9UpkwiqWfdqqt2iVJxH'
-});
 
+var client = new Twitter({
+  consumer_key: 'YourOwn',
+  consumer_secret: 'YourOwn',
+  access_token_key: 'YourOwn',
+  access_token_secret: 'YourOwn'
+});
 
 
 
